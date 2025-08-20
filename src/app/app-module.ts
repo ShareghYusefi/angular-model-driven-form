beaae26ba2c50de8a25f54b9b46d5e730746eaca
Sharegh Yusefi <ShareghYusefi@gmail.com>
2025-08-19 19:21:04 -0600

    bootstrap & form

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { FormComponent } from './form-component/form-component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    FormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-component/form-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-component',
+  standalone: false,
+  templateUrl: './form-component.html',
+  styleUrl: './form-component.css',
+})
+export class FormComponent {}
